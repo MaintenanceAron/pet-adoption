d0d60b3d09fb80b83b7d6a10d2e68d970328242e
Aron <kipkoecharonz@gmail.com>
2024-04-03 12:10:52 -0400

    added filter to live

--- main.js
+++ main.js
@@ -5,12 +5,11 @@ async function start() {
   const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
   const weatherData = await weatherPromise.json()
 
-  const ourTemparature = weatherData.properties.periods[0].temperature
-
-  document.querySelector("#temperature-output").textContent = ourTemparature
-
+  const ourTemperature = weatherData.properties.periods[0].temperature
+  document.querySelector("#temperature-output").textContent = ourTemperature
 
 }
+
 start()
 
 async function petsArea() {
@@ -19,13 +18,17 @@ async function petsArea() {
   petsData.forEach(pet => {
     const clone = template.content.cloneNode(true)
 
-    clone.querySelector("h3").textContent = pet.name
+    clone.querySelector(".pet-card").dataset.species = pet.species
 
+    clone.querySelector("h3").textContent = pet.name
     clone.querySelector(".pet-description").textContent = pet.description
     clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
+
     if (!pet.photo) pet.photo = "images/fallback.jpg"
+
     clone.querySelector(".pet-card-photo img").src = pet.photo
     clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}.`
+
     wrapper.appendChild(clone)
   })
   document.querySelector(".list-of-pets").appendChild(wrapper)
@@ -38,13 +41,12 @@ function createAgeText(birthYear) {
   const age = currentYear - birthYear
 
   if (age == 1) return "1 year old"
-
-  if (age == 0) return "Less than a year Old"
+  if (age == 0) return "Less than a year old"
 
   return `${age} years old`
 }
 
-//pet filter button code
+// pet filter button code
 const allButtons = document.querySelectorAll(".pet-filter button")
 
 allButtons.forEach(el => {
@@ -54,7 +56,17 @@ allButtons.forEach(el => {
 function handleButtonClick(e) {
   // remove active class from any and all buttons
   allButtons.forEach(el => el.classList.remove("active"))
-  //add active class to the specific button that ahs got clicked
+
+  // add active class to the specific button that just got clicked
   e.target.classList.add("active")
-  //actuall filter the pets down below
+
+  // actually filter the pets down below
+  const currentFilter = e.target.dataset.filter
+  document.querySelectorAll(".pet-card").forEach(el => {
+    if (currentFilter == el.dataset.species || currentFilter == "all") {
+      el.style.display = "grid"
+    } else {
+      el.style.display = "none"
+    }
+  })
 }
